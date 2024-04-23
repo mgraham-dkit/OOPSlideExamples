e6a34576d2c232af185cb74f2342abd1c852494a
michelle <michelle.graham@dkit.ie>
2024-04-23 01:44:39 +0100

    Added sample code for basic examples of working with locks.

---
+++ g_threading/race_conditions_and_locking/demonstrating_race_conditions.py
@@ -0,0 +1,45 @@
+import time
+import logging
+from concurrent.futures import ThreadPoolExecutor
+from threading import current_thread
+
+log_format = "%(threadName)s - %(asctime)s: %(message)s"
+logging.basicConfig(format=log_format, level=logging.INFO, datefmt="%H:%M:%S")
+
+
+class Counter:
+    logger = logging.getLogger(__name__)
+
+    def __init__(self, max_value: int | float):
+        if not isinstance(max_value, int) and not isinstance(max_value, float):
+            Counter.logger.error(f"Inappropriate type supplied for max_value: {type(max_value)} : {max_value}")
+            raise TypeError("Value must be numeric")
+
+        if max_value < 0:
+            Counter.logger.error(f"Inappropriate value supplied for max_value: supplied {max_value} but > 0 required")
+            raise ValueError("Maximum value must be >= 0")
+
+        self.max_value = max_value
+        self.value = 0
+
+    def increment(self, id_num: int) -> None:
+        # Set name of thread currently running this counter
+        this_thread = current_thread()
+        this_thread.name = f"Thread {id_num}"
+
+        Counter.logger.info(f"Beginning increment action - Value: {self.value}")
+        time.sleep(0.1)
+        local_value = self.value
+        local_value += 1
+
+        time.sleep(0.01)
+        self.value = local_value
+        Counter.logger.info(f"Ending increment action - Value: {self.value}")
+
+
+if __name__ == "__main__":
+    counter = Counter(15)
+
+    with ThreadPoolExecutor(max_workers=5) as executor:
+        for i in range(2):
+            executor.submit(counter.increment, i)
---
+++ g_threading/race_conditions_and_locking/improved_addressing_race_conditions.py
@@ -0,0 +1,51 @@
+import time
+import logging
+from concurrent.futures import ThreadPoolExecutor
+from threading import current_thread, Lock
+
+log_format = "%(threadName)s - %(asctime)s: %(message)s"
+logging.basicConfig(format=log_format, level=logging.INFO, datefmt="%H:%M:%S")
+
+
+class Counter:
+    logger = logging.getLogger(__name__)
+
+    def __init__(self, max_value: int | float):
+        if not isinstance(max_value, int) and not isinstance(max_value, float):
+            Counter.logger.error(f"Inappropriate type supplied for max_value: {type(max_value)} : {max_value}")
+            raise TypeError("Value must be numeric")
+
+        if max_value < 0:
+            Counter.logger.error(f"Inappropriate value supplied for max_value: supplied {max_value} but > 0 required")
+            raise ValueError("Maximum value must be >= 0")
+
+        self.max_value = max_value
+        self.value = 0
+        self.lock = Lock()
+
+    def increment(self, id_num: int) -> None:
+        # Set name of thread currently running this counter
+        this_thread = current_thread()
+        this_thread.name = f"Thread {id_num}"
+
+        Counter.logger.info(f"Beginning increment action - Value: {self.value}")
+        # Add some sleep in, so we can get the threads to interweave a bit better
+        time.sleep(0.1)
+        # Lock access to the shared value
+        # Use a context manager to ensure the lock is released if something goes wrong
+        with self.lock:
+            local_value = self.value
+            local_value += 1
+            self.value = local_value
+
+        Counter.logger.info(f"Ending increment action - Value: {self.value}")
+
+
+if __name__ == "__main__":
+    counter = Counter(15)
+
+    with ThreadPoolExecutor(max_workers=5) as executor:
+        for i in range(20):
+            executor.submit(counter.increment, i)
+
+    print("Program terminated.")
